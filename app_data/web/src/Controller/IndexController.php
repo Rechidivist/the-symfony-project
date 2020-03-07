3ba4f4c54ea31200c0e9fd1c7323a83c4026ab52
Denis Beloedoff <den.beloedoff2011@yandex.ru>
2020-03-07 20:48:45 +0300

    Добавление начальной страницы

---
+++ app_data/web/src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        $entityManager = $this->getDoctrine()->getRepository(\App\Entity\Lamps::class)->findAll();
+
+
+
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
