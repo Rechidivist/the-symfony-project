e3da304a88ec4cb44b7de000e3fe037d948e67e4
Denis Beloedoff <den.beloedoff2011@yandex.ru>
2020-03-09 11:19:32 +0300

    Убираем лишнюю фигню

--- app_data/web/src/Controller/IndexController.php
+++ app_data/web/src/Controller/IndexController.php
@@ -12,9 +12,6 @@ class IndexController extends AbstractController
      */
     public function index()
     {
-        $entityManager = $this->getDoctrine()->getRepository(\App\Entity\Lamps::class)->findAll();
-
-
 
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
